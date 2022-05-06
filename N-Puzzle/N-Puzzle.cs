b63233d2e8b8e488069f56c1b3474fc70eeb3e41
omarkhaled646 <63152184+omarkhaled646@users.noreply.github.com>
2022-05-06 14:39:07 +0200

    initial commit

---
+++ N-Puzzle/N-Puzzle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Puzzle
+{
+    class N_Puzzle
+    {
+       public static int solvePuzzle(int size , int[,] puzzle)
+        {
+            return 0;
+        }
+    }
+
+}
